b2a39faeb97b0623afd6c78e7ad8d92bf5ff5fed
Ian Pollack <ianjpollack@gmail.com>
2018-04-01 20:50:54 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,10 +1,16 @@
 require "pry"
 
 def alphabetize(arr)
+  binding.pry
   espe_alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+  binding.pry
   arr.sort_by do |phrase|
+    binding.pry
     phrase.chars.map do |i|
+      binding.pry
       espe_alpha.index(i)
+      binding.pry
     end
   end
+  binding.pry
 end
