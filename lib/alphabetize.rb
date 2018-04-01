4a67d746227742a1a6536d67e24bebe401c18d65
Ian Pollack <ianjpollack@gmail.com>
2018-04-01 21:27:04 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,14 +2,9 @@ require "pry"
 
 def alphabetize(arr)
   espe_alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
-    super_new_array = arr.sort_by do |phrase|
-      new_array = phrase.chars.map do |i|
-      #  binding.pry
+    arr.sort_by do |phrase|
+      phrase.chars.map do |i|
         espe_alpha.index(i)
       end
-
-  #   binding.pry
     end
-puts super_new_array
 end
-alphabetize(["mia amas vin", "mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"])
